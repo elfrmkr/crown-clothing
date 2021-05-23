676be4f5c52e7b4114ecee3d3a73b532d8f0466f
ME <you@example.com>
2021-05-23 11:14:26 +0300

    updated menu items to navigate a page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
 
+const HatsPage = ()=> (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+//exact will keep away all the matched URL showing up in the same page
+// the moment when switch sees something matched, it doesn't render the rest.
 function App() {
   return (
     <div>
-       <HomePage/>
+      <Switch>
+        <Route exact path ='/' component = {HomePage}/>
+        <Route path ='/hats' component = {HatsPage}/>
+      </Switch>
     </div>
   );
 }
