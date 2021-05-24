680daeb6a2497323ae726eef8a0c8784a560cf8c
ME <you@example.com>
2021-05-24 10:33:38 +0300

    adding header and links to homepage, shop and contact

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 //exact will keep away all the matched URL showing up in the same page
 // the moment when switch sees something matched, it doesn't render the rest.
+// header is always present and rendered in this location
 function App() {
   return (
-    <div>
+    <div>  
+      <Header/>
       <Switch>
         <Route exact path ='/' component = {HomePage}/>
         <Route path ='/shop' component = {ShopPage}/>
