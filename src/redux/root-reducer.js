69242787f96a5afd43e729dd846d059bb9bd1c39
ME <you@example.com>
2021-05-28 16:06:59 +0300

    added cart icon and cart dropdown

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 // represents all of the sate of the app, combines all of the other states together
 import {combineReducers} from 'redux';
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
