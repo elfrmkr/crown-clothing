0f03c1903860c09890d805394fb3142e126ab1a8
elfrmkr <elfrmkr98@gmail.com>
2021-05-31 15:17:08 +0300

    updating app with reselect; memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({cartItems}) => (
     <div className = 'cart-dropdown'>
@@ -14,8 +15,8 @@ const CartDropdown = ({cartItems}) => (
         <CustomButton> GO TO CHECKOUT</CustomButton>
     </div>
 );
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
 });
 
 
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,15 +3,25 @@ import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
 
-const CartIcon = ({toggleCartHidden}) =>(
+const CartIcon = ({toggleCartHidden, itemCount}) =>(
     <div className = 'cart-icon' onClick = {toggleCartHidden} >
         <ShoppingIcon className ='shopping-icon '/>
-        <span className = 'item-count'>0</span>
+        <span className = 'item-count'>{itemCount}</span>
     </div>
 );
 
 const mapDispatchToProps = dispatch =>({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
-export default connect(null, mapDispatchToProps) (CartIcon);
+/* reduce -> native array method.
+this operation also called selector, pulls of a slice of the state
+- reduce is always returning a new value, always rendered everytime the state changes --> we don't want that, unrelated changes makes the function fired again.*/
+
+/*u should do cashing of the selector value. If the items that the functions care about didn't change, no need for rendering( memoization )*/
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount
+});
+export default connect(mapStateToProps, mapDispatchToProps) (CartIcon);
