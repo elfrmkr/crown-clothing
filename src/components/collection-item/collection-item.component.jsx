0bb046b75e24f15a9b544cddd924889f67362c18
elfrmkr <elfrmkr98@gmail.com>
2021-12-15 22:33:15 +0300

    feat: started to add toasts after an successful event

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,29 +1,56 @@
-import React from 'react';
-import './collection-item.styles.scss';
-import CustomButton from '../custom-button/custom-button.component';
-import {connect} from 'react-redux';
-import {addItem} from '../../redux/cart/cart.actions';
+import React from "react";
+import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
+import { ToastMessage } from "../../toasts/toast";
+import { ToastContainer } from "react-toastify";
 
-const CollectionItem = ({item, addItem}) => {
-    // we are doing this because we want these values to use still
-    const { name, price,imageUrl} = item;
-    return(
-    <div className = 'collection-item'> 
-        <div
-        className = 'image'
-        style = {{ backgroundImage:`url(${imageUrl})` }}  />
-            <div className = 'collection-footer'>
-                <span className = 'name'>{name}</span>
-                <span className = 'price'>${price}</span>   
-        </div>
-        <CustomButton className = 'custom-button'  onClick = {() => addItem(item)} inverted> ADD TO CHART</CustomButton>
+const CollectionItem = ({ item, addItem }) => {
+  const notify = React.useCallback((type, message) => {
+    ToastMessage({ type, message });
+  }, []);
+
+  const dismiss = React.useCallback(() => {
+    ToastMessage.dismiss();
+  }, []);
+
+  // we are doing this because we want these values to use still
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">${price}</span>
+      </div>
+      <CustomButton
+        className="custom-button"
+        onClick={() => {
+          addItem(item);
+          notify("success", "Success!");
+        }}
+        inverted
+      >
+        {" "}
+        ADD TO CHART
+      </CustomButton>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={4000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        draggable={false}
+        pauseOnVisibilityChange
+        closeOnClick
+        pauseOnHover
+      />
     </div>
-    )
+  );
 };
 
-const mapDispatchToProps = dispatch =>({
-
-    addItem: item => dispatch(addItem(item))
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
 });
 
-export default connect(null,mapDispatchToProps)(CollectionItem);
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyBXvI77atkGv5m0DAgQ_NRwIdtdydbBHsE",
-    authDomain: "crwn-db-2717a.firebaseapp.com",
-    projectId: "crwn-db-2717a",
-    storageBucket: "crwn-db-2717a.appspot.com",
-    messagingSenderId: "24811549808",
-    appId: "1:24811549808:web:1ef1362e9be9419473f0b5",
-    measurementId: "G-TEB625RXLK"
-  };
+  apiKey: "AIzaSyBXvI77atkGv5m0DAgQ_NRwIdtdydbBHsE",
+  authDomain: "crwn-db-2717a.firebaseapp.com",
+  projectId: "crwn-db-2717a",
+  storageBucket: "crwn-db-2717a.appspot.com",
+  messagingSenderId: "24811549808",
+  appId: "1:24811549808:web:1ef1362e9be9419473f0b5",
+  measurementId: "G-TEB625RXLK",
+};
 
 // taking the user object and store it, asynch -> making an API request
 // it should work when a user sign in, getting an actual object not null, making qurey: asking firebase a doc or collection
@@ -19,76 +19,78 @@ const config = {
 Firebase can give two objects: 
 1) Query reference -> references the current place of the object
 2) Query Snapshot*/
-export const createUserProfileDocument =
-async(userAuth, additionalData) => {
-  if(!userAuth) return;
-// with this you can see that if the property actually exists from , snapshot represents the data
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const collectionRef = firestore.collection('users');
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  // with this you can see that if the property actually exists from , snapshot represents the data
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const collectionRef = firestore.collection("users");
 
-    const snapShot = await userRef.get();
-    const collectionSnapshot = await collectionRef.get();
-   // console.log({collection: collectionSnapshot.docs.map(doc => doc.data())});
+  const snapShot = await userRef.get();
+  const collectionSnapshot = await collectionRef.get();
+  // console.log({collection: collectionSnapshot.docs.map(doc => doc.data())});
 
-// in order to crate the object that isn't stored yet, you need to use reference
-    if(!snapShot.exists)
-    {
-      const {displayName, email} = userAuth;
-      const createAt = new Date();
-// async request to database to store data, .set being the create method
-      try{
-        await userRef.set({
-          displayName,
-          email,
-          createAt,
-          ...additionalData
-        })
-      }catch (error){
-          console.log("error creating user", error.message);
-          }
+  // in order to crate the object that isn't stored yet, you need to use reference
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+    // async request to database to store data, .set being the create method
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
     }
-    return userRef;
-  };
+  }
+  return userRef;
+};
 
-  // each call is individual in firestore, we need to batch (or group) all of the calls together
+// each call is individual in firestore, we need to batch (or group) all of the calls together
 
-export const addCollectionAndDocuments = async(collectionKey, objectsToAdd) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
   const collectionRef = firestore.collection(collectionKey);
   // in the collection, we will make new document object with unique key
   const batch = firestore.batch();
-  objectsToAdd.forEach(obj => {
-  const newDocRef = collectionRef.doc();
-  batch.set(newDocRef, obj);
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
   });
-// will fire off the batch request, when commit succeeds, it will come back with a null value, it will need to return from the call, make it async fnc
+  // will fire off the batch request, when commit succeeds, it will come back with a null value, it will need to return from the call, make it async fnc
   return await batch.commit();
 };
 
-  export const converCollectionsSnapshotToMap = (collections) =>{
-    const transformedCollections = collections.docs.map(doc => {
-      const {title, items} = doc.data();
-      return {
-        routeName: encodeURI(title.toLowerCase()),
-        id: doc.id,
-        title,
-        items
-      }
-    });
+export const converCollectionsSnapshotToMap = (collections) => {
+  const transformedCollections = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
   // initial object goes through first collection. Empty object with the property of hats e.g equals to the hats collection and it returns that object and goes to the second object i.e. jackets
-  return transformedCollections.reduce((accumulator, collection) =>{
+  return transformedCollections.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
-    return accumulator
-  }, {})
-  };
+    return accumulator;
+  }, {});
+};
 
+firebase.initializeApp(config);
 
-  firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(provider);
+};
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt : 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
-  export default firebase;
+export default firebase;
